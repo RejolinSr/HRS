7ddf08c0b6612765a31ccc225bc3fe1792351016
RejolinSr <122664933+RejolinSr@users.noreply.github.com>
2023-01-14 13:42:40 +0530

    scsd

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const usermodel = require("../model/user")
+app.use(express.json());
+// const { request, response } = require("express");
+
+const indexcontrolller = require("./controller/index");
+
+
+app.use("/rejo",usermodel.User);
+
+
+
+app.use("/name", indexcontrolller.getuser); 
+app.get ("/",(request,response)=>{
+    return response.send("hgehcebvcv")
+});
+
+
+
+module.exports = app;
